01e3dd9ed7d3e5c7ed68f7035d75a1c9712ad1f5
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-02-20 17:41:02 +0700

    update delete user

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
 const router = express.Router();
-import * as userController from "../controllers/userController.js";
+import userController from "../controllers/userController.js";
 
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
 router.put("/update-user/:id", userController.updateUser);
+router.delete("/delete-user/:id", userController.deleteUser);
 
 export default router;
-
-
